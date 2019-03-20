52dc038787365e7df1819da96491e334e274e2cf
Tammy Metz <tammy@tammymetz.com>
2019-03-20 22:56:26 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,4 +1,12 @@
 class SecretsController < ApplicationController
+  before_action :require_login
+  
   def new
+  end
+
+  private
+
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
   end
 end
