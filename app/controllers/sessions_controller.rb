d8d43c4d2080ac6b7ebe40911cd9e31bcc53c71d
Tammy Metz <tammy@tammymetz.com>
2019-03-20 22:59:41 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,17 @@ class SessionsController < ApplicationController
   def new
   end
 
-  private
-
-  def current_user
-
+  def create
+    if (!params[:current_user] || params[:current_user].empty?)
+      redirect_to controller: 'sessions', action: 'new'
+    else
+      session[:current_user] = params[:current_user]
+      redirect_to '/'
+    end
   end
+
+  def destroy
+    session.delete :current_user
+  end
+  
 end
